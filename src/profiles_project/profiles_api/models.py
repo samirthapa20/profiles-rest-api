005667e1274fcbdc8829d85573ca59f8fcc0136c
Samir Thapa <samirthapa20@gmail.com>
2018-10-26 21:16:21 +0545

    Django admin account created

--- src/profiles_project/profiles_api/models.py
+++ src/profiles_project/profiles_api/models.py
@@ -2,50 +2,58 @@ from django.db import models
 from django.contrib.auth.models import AbstractBaseUser
 from django.contrib.auth.models import PermissionsMixin
 from django.contrib.auth.models import BaseUserManager
-
 # Create your models here.
 
 class UserProfileManager(BaseUserManager):
-	'''Helps Djangp work wih our custom user model'''
+	'''Helps Django work with our custom user model.'''
 	def create_user(self,email,name,password=None):
-		'''creates a new user profile object'''
+		'''Creates a new user profile object.'''
 		if not email:
 			raise ValueError("Users must have an email address.")
 
-		email = self.normalize_email(email)
+		email =self.normalize_email(email)
 		user = self.model(email=email,name=name)
+
 		user.set_password(password)
 		user.save(using=self._db)
+
 		return user
 
+
 	def create_superuser(self,email,name,password):
-		'''Creates and saves a new superuser with given details'''
+		'''Ceates and saves a new superuser wih given details.'''
 		user = self.create_user(email,name,password)
 
-		user.is_superuser= True
+		user.is_superuser = True
 		user.is_staff = True
+
 		user.save(using=self._db)
+
 class UserProfile(AbstractBaseUser,PermissionsMixin):
-	'''Represents a 'user profile' inside our system'''
+	'''Represent a user profile inside our system'''
 
 	email = models.EmailField(max_length=255,unique=True)
 	name = models.CharField(max_length=255)
 	is_active = models.BooleanField(default=True)
 	is_staff = models.BooleanField(default=False)
+
 	objects = UserProfileManager()
 
 	USERNAME_FIELD = 'email'
-	REQUIRED_FEILDS = ['name']
+	REQUIRED_FIELDS = ['name']
 
 	def get_full_name(self):
-		'''Used to get users full name.'''
+		'''Used to get a users full name.'''
+
 		return self.name
 
 	def get_short_name(self):
 		'''Used to get a users short name.'''
+
 		return self.name
 
 	def __str__(self):
-		'''django uses this when it needs to convert the object to a string'''
+		'''Django uses this when it needs to convert the object to string'''
+
 		return self.email
 
