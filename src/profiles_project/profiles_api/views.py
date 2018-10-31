546b14fe08e129c2e2a705d1a533db79c6b2f1d3
Samir Thapa <samirthapa20@gmail.com>
2018-10-31 21:16:16 +0545

    Added improved permissions to use profile feed items api

--- src/profiles_project/profiles_api/views.py
+++ src/profiles_project/profiles_api/views.py
@@ -7,6 +7,8 @@ from rest_framework.authentication import TokenAuthentication
 from rest_framework import filters
 from rest_framework.authtoken.serializers import AuthTokenSerializer
 from rest_framework.authtoken.views import ObtainAuthToken
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
+from rest_framework.permissions import IsAuthenticated
 from . import serializers
 from . import models
 from . import permissions
@@ -119,3 +121,15 @@ class LoginViewSet(viewsets.ViewSet):
 	def create(self,request):
 		'''Use the ObtainAuthToken APIView to validate and create a token.'''
 		return ObtainAuthToken().post(request)
+
+
+class UserProfileFeedViewSet(viewsets.ModelViewSet):
+	'''Handles creating, reading and updating profile feed items.'''
+	authentication_classes = (TokenAuthentication,)
+	serializer_class = serializers.ProfileFeedItemSerializer
+	queryset = models.ProfileFeedItem.objects.all()
+	permission_classes = (permissions.PostOwnStatus, IsAuthenticated)
+
+	def perform_create(self,serializer):
+		'''Sets the user profile to the logged in user.'''
+		serializer.save(user_profile=self.request.user)
